1b28d13d7c57dbca30048a4cea8b373859f84028
Beau Gunderson <beau@beaugunderson.com>
2014-07-17 21:56:54 -0700

    WIP: Scaffold out the front page

--- open_humans/urls.py
+++ open_humans/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'open_humans.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
 )
