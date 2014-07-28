e62914e1cd8f82037ed1451a46a1723c0733127a
Beau Gunderson <beau@beaugunderson.com>
2014-07-28 15:26:07 -0700

    Add WIP login form, set up URLs

--- open_humans/urls.py
+++ open_humans/urls.py
@@ -2,8 +2,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
+    url(r'^dashboard/$', TemplateView.as_view(template_name='dashboard.html')),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}),
+
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
 )
