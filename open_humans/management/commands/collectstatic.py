fc269a85a894a7c0bfa504a20cedd98c60b23649
Beau Gunderson <beau@beaugunderson.com>
2014-07-29 20:36:17 -0700

    Add Heroku-specific code for collectstatic

--- open_humans/management/commands/collectstatic.py
+++ open_humans/management/commands/collectstatic.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from django.contrib.staticfiles.management.commands.collectstatic import \
@@ -5,16 +6,29 @@ from django.contrib.staticfiles.management.commands.collectstatic import \
 
 
 class Command(BaseCommand):
+    """
+    A version of collectstatic that runs `gulp build --production` first.
+    """
     def handle(self, *args, **options):
-        if options['dry_run']:
-            self.stdout.write('Not running gulp because of --dry-run')
-        else:
-            self.stdout.write('Starting gulp...')
+        if not options['dry_run']:
+            popen_args = {
+                "shell": True,
+                "stdin": subprocess.PIPE,
+                "stdout": self.stdout,
+                "stderr": self.stderr
+            }
 
-        subprocess.Popen(['gulp build --production'],
-                         shell=True,
-                         stdin=subprocess.PIPE,
-                         stdout=self.stdout,
-                         stderr=self.stderr).wait()
+            # HACK: This command is executed without node_modules in the PATH
+            # when it's executed from Heroku... Ideally we wouldn't need any
+            # Heroku-specific code for this to work.
+            if os.path.exists('/app/requirements.txt'):
+                popen_args['env'] = {
+                    'PATH': (os.environ['PATH'] +
+                             ':/app/node_modules/.bin' +
+                             ':/app/vendor/node/bin')
+                }
+
+            subprocess.Popen(['gulp build --production'],
+                             **popen_args).wait()
 
         super(Command, self).handle(*args, **options)
