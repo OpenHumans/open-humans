dfa76a4ad4a15e4068135b5f82ef5a00763c4b57
Beau Gunderson <beau@beaugunderson.com>
2014-07-31 12:45:32 -0700

    Add Profile model and post-save hook

---
+++ open_humans/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    about_me = models.TextField()
+
+
+@receiver(post_save, sender=User, dispatch_uid='create_profile')
+def cb_create_profile(sender, instance, created, raw, **kwargs):
+    """
+    Create an account for the newly created user.
+    """
+    # If we're loading a user via a fixture then `raw` will be true and in that
+    # case we won't want to create a Profile to go with it
+    if created and not raw:
+        Profile.objects.create(user=instance)
