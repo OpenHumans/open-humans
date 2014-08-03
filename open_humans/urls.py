066c35b77ec662d75d94eb6520474cc192d75a8e
Beau Gunderson <beau@beaugunderson.com>
2014-08-03 15:37:24 -0400

    Cleanup, add WIP profile views

--- open_humans/urls.py
+++ open_humans/urls.py
@@ -1,24 +1,34 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 
-from .views import UserCreateView
+from .views import UserCreateView, UserProfileDetailView, UserProfileEditView
 
 urlpatterns = patterns(
     '',
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
-    url(r'^dashboard/$', TemplateView.as_view(template_name='dashboard.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^dashboard/$', TemplateView.as_view(template_name='dashboard.html'),
+        name='dashboard'),
 
-    url(r'^accounts/signup/$', UserCreateView.as_view(), name='auth_signup'),
+    url(r'^accounts/signup/$', UserCreateView.as_view(),
+        name='accounts-signup'),
 
-    url(r'^accounts/login/$', auth_views.login,
-        {'template_name': 'home.html'}, name='auth_login'),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'home.html'},
+        name='accounts-login'),
 
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'},
-        name='auth_logout'),
+        name='accounts-logout'),
+
+    url(r'^profile/$', login_required(UserProfileDetailView.as_view()),
+        name='profile-detail'),
+
+    url(r'^profile/edit/$', login_required(UserProfileEditView.as_view()),
+        name='profile-edit'),
 )
