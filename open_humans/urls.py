951d0de12ea57edbee4d6c5d6c734b96e5946003
Madeleine Price Ball <mad-github@printf.net>
2014-08-02 21:44:37 -0400

    Actually might be nice to have a login page...
    Disconcerting to jump to the front page modal when following
    a "log in" link from a different page.

--- open_humans/urls.py
+++ open_humans/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns(
     url(r'^accounts/signup/$', UserCreateView.as_view(), name='auth_signup'),
 
     url(r'^accounts/login/$', auth_views.login,
-        {'template_name': 'home.html'}, name='auth_login'),
+        {'template_name': 'login.html'}, name='auth_login'),
 
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'},
         name='auth_logout'),
