3af65be4e30a6ee21a9f08df14b8b8ea03df61e6
Beau Gunderson <beau@beaugunderson.com>
2014-08-05 20:21:51 -0400

    Use crispy-forms to render profile-edit

--- open_humans/views.py
+++ open_humans/views.py
@@ -1,7 +1,10 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
-from django.forms import BooleanField
+from django.forms import BooleanField, ModelForm
 from django.http import HttpResponseRedirect
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
@@ -18,20 +21,46 @@ class UserProfileDetailView(DetailView):
     template_name = 'profile/detail.html'
 
     def get_object(self, queryset=None):
-        return self.request.user
+        return self.request.user.profile
+
+
+class ProfileEditForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileEditForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+
+        self.helper.form_id = 'edit-form'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+
+        self.helper.layout = Layout(
+            'about_me',
+        )
+
+        self.helper.add_input(Submit('save', 'Save'))
+
+    class Meta:
+        model = Profile
+        # XXX: When the next version of crispy-forms comes out this duplication
+        # should no longer be necessary.
+        fields = ('about_me',)
 
 
 class UserProfileEditView(UpdateView):
     """
     An edit view of the current user's profile.
     """
-    context_object_name = 'profile'
+    form_class = ProfileEditForm
     model = Profile
     template_name = 'profile/edit.html'
-    success_url = reverse_lazy('profile-edit')
+    success_url = reverse_lazy('profile-detail')
 
     def get_object(self, queryset=None):
-        return self.request.user
+        return self.request.user.profile
 
 
 class UserWithTermsForm(UserCreationForm):
