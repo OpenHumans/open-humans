364471fc876da0be9ef4e11a3db6022246b138dd
Madeleine Ball <mpball@gmail.com>
2014-10-17 13:06:01 -0400

    Add 23andme completion modal

--- static/js/profile-research-data.js
+++ static/js/profile-research-data.js
@@ -3,3 +3,13 @@
 $(function () {
   $("[data-toggle='popover']").popover({html:true, trigger:'focus'});
 });
+
+$(function () {
+  // JavaScript to enable link to modal
+  var url = document.location.toString();
+
+  if (url.match('#')) {
+      console.log(url)
+    $('#' + url.split('#')[1]).modal('show');
+  }
+});
