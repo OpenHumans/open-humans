1d30e3c9be05724c40e7dc2d715c4eb74d35b536
Madeleine Ball <mpball@gmail.com>
2014-11-02 21:58:56 -0500

    Fix TaskUpdateView to work with POST and only POST

--- activities/views.py
+++ activities/views.py
@@ -10,30 +10,29 @@ from .models import TASK_SUCCESSFUL, TASK_FAILED
 
 
 class TaskUpdateView(View):
+    """Receive and record task success/failure input."""
 
-    # GET only for testing purposes.
-    @csrf_exempt
-    def get(self, request, *args, **kwargs):
-        task_name = request.GET['name']
-        task_state = request.GET['state']
-        s3_key_name = request.GET['s3_key_name']
-        return self.update_task(task_name, task_state, s3_key_name)
-
-    @csrf_exempt
     def post(self, request, *args, **kwargs):
         task_name = request.POST['name']
         task_state = request.POST['state']
         s3_key_name = request.POST['s3_key_name']
         return self.update_task(task_name, task_state, s3_key_name)
 
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        super(TaskUpdateView, self).dispatch(*args, **kwargs)
+
     @staticmethod
     def update_task(task_name, task_state, s3_key_name):
         task_data = None
         if task_name == 'client.start_23andme_ohdataset':
-            task_data = DataExtractionTask23andme.objects.get(
-                data_file__file=s3_key_name)
+            try:
+                task_data = DataExtractionTask23andme.objects.get(
+                    data_file__file=s3_key_name)
+            except DataExtractionTask23andme.DoesNotExist:
+                pass
         if not task_data:
-            return HttpResponse('Invalid task name!')
+            return HttpResponse('Invalid task and key name data!')
 
         if task_state == 'SUCCESS':
             task_data.status = TASK_SUCCESSFUL
