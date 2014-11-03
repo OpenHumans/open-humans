f7a3f9a0ed60562feb069650415adcecb4c751c8
Madeleine Ball <mpball@gmail.com>
2014-11-02 22:05:17 -0500

    Move 23andme data import to occur on profile research data
    This lets us check if 23andme data sets were imported, using this view's data_sets.

--- static/js/profile-research-data.js
+++ static/js/profile-research-data.js
@@ -1,5 +1,14 @@
 /*globals $:true*/
 
+var url = document.location.toString();
+
 $(function () {
   $("[data-toggle='popover']").popover({html:true, trigger:'focus'});
+});
+
+// So we can link to this modal directly from the 'activities' page.
+$(function () {
+  if (url.match('#add-data-23andme-modal')) {
+    $('#add-data-23andme-modal').removeClass('fade').modal('show');
+  }
 });
