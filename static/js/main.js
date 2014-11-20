f0e19248a8c2b50c1e98873f252182ae1829dc32
Madeleine Ball <mpball@gmail.com>
2014-11-20 10:41:47 -0500

    Add CSRF for Ajax
    Following instructions from:
    https://docs.djangoproject.com/en/1.7/ref/contrib/csrf/#ajax

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,21 @@
 /*globals $:true*/
 
+'use strict';
+
 $(function () {
 
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+  var csrftoken = $.cookie('csrftoken');
+
+  $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+          if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader('X-CSRFToken', csrftoken);
+          }
+      }
+  });
 });
