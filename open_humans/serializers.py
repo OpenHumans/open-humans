a1318a5ced6efc4ae88abc0b23190daea5899704
Beau Gunderson <beau@beaugunderson.com>
2014-11-24 23:04:31 -0800

    Make /api/profile return no private data

--- open_humans/serializers.py
+++ open_humans/serializers.py
@@ -1,14 +1,19 @@
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+# from django.core.urlresolvers import reverse
 from rest_framework import serializers
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    url = serializers.SerializerMethodField('get_profile_url')
+    # url = serializers.SerializerMethodField('get_profile_url')
+    message = serializers.SerializerMethodField('get_message')
 
     class Meta:
         model = User
-        fields = ('id', 'url', 'username')
+        # fields = ('id', 'url', 'username')
+        fields = ('message',)
 
-    def get_profile_url(self, obj):
-        return reverse('member_profile', args=(obj.id,))
+    # def get_profile_url(self, obj):
+    #     return reverse('member_profile', args=(obj.id,))
+
+    def get_message(self, obj):
+        return 'profiles are not yet implemented'
