97d154b94c4e020647f08add403c134d2c9c9112
Beau Gunderson <beau@beaugunderson.com>
2015-01-04 22:13:09 -0800

    dj_static → whitenoise for static files

--- open_humans/wsgi.py
+++ open_humans/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "open_humans.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
