3fc108d394d4505d107d562cb274319902a6cc3a
Beau Gunderson <beau@beaugunderson.com>
2015-01-06 12:34:47 -0800

    Add content type migration for profile → member

---
+++ open_humans/migrations/0005_rename_content_type.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+from django.conf import settings
+
+sql = """UPDATE django_content_type
+         SET name = "member",
+             model = "member"
+         WHERE name = "profile" AND
+               model = "profile" AND
+               app_label = "open_humans";"""
+
+reverse_sql = """UPDATE django_content_type
+                 SET name = "profile",
+                     model = "profile"
+                 WHERE name = "member" AND
+                       model = "member" AND
+                       app_label = "open_humans";"""
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('open_humans', '0004_auto_20150106_1828'),
+    ]
+
+    operations = [
+        migrations.RunSQL(sql, reverse_sql)
+    ]
