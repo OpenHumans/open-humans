1e741524a72d6472d9fd9f0c4364b21a5b411f7d
Beau Gunderson <beau@beaugunderson.com>
2015-01-06 12:38:59 -0800

    Update strings for postgresql

--- open_humans/migrations/0005_rename_content_type.py
+++ open_humans/migrations/0005_rename_content_type.py
@@ -5,18 +5,18 @@ from django.db import migrations
 from django.conf import settings
 
 sql = """UPDATE django_content_type
-         SET name = "member",
-             model = "member"
-         WHERE name = "profile" AND
-               model = "profile" AND
-               app_label = "open_humans";"""
+         SET name = 'member',
+             model = 'member'
+         WHERE name = 'profile' AND
+               model = 'profile' AND
+               app_label = 'open_humans';"""
 
 reverse_sql = """UPDATE django_content_type
-                 SET name = "profile",
-                     model = "profile"
-                 WHERE name = "member" AND
-                       model = "member" AND
-                       app_label = "open_humans";"""
+                 SET name = 'profile',
+                     model = 'profile'
+                 WHERE name = 'member' AND
+                       model = 'member' AND
+                       app_label = 'open_humans';"""
 
 
 class Migration(migrations.Migration):
