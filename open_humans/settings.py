970e7447c2946a24ee701632b2e07c2f2789083f
Beau Gunderson <beau@beaugunderson.com>
2015-01-11 14:18:26 -0800

    Redirect http:// to https://

--- open_humans/settings.py
+++ open_humans/settings.py
@@ -100,6 +100,8 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'sslify.middleware.SSLifyMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
