a2e7fe7d5e4ba6ba8c42ba6984bc0cd895611835
Beau Gunderson <beau@beaugunderson.com>
2015-01-14 15:42:45 -0800

    Move logout JS to main.js, fix race condition
    Fixes #36

--- static/js/main.js
+++ static/js/main.js
@@ -2,20 +2,27 @@
 
 'use strict';
 
-$(function () {
+function csrfSafeMethod(method) {
+  // These HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
 
-  function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-  }
+var csrfToken = $.cookie('csrftoken');
 
-  var csrftoken = $.cookie('csrftoken');
-
-  $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-        xhr.setRequestHeader('X-CSRFToken', csrftoken);
-      }
+$.ajaxSetup({
+  beforeSend: function (xhr, settings) {
+    if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader('X-CSRFToken', csrfToken);
     }
+  }
+});
+
+$(function () {
+  $('.logout-link').click(function (e) {
+    e.preventDefault();
+
+    $.post($(this).attr('href'), function () {
+      location.reload();
+    });
   });
 });
