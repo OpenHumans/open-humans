9087d8420bbeee4cc397f6c03253c6fc1eed5b74
Madeleine Ball <mpball@gmail.com>
2015-01-27 20:55:25 -0500

    📐 Equalize div heights on activities so buttons vertically align

--- static/js/activities.js
+++ static/js/activities.js
@@ -2,6 +2,15 @@
 
 var $ = require('jquery');
 
+// From example code here: http://api.jquery.com/map/
+$.fn.equalizeHeights = function() {
+  var maxHeight = this.map(function( i, e ) {
+    return $( e ).height();
+  }).get();
+  return this.height( Math.max.apply( this, maxHeight ) );
+};
+
 $(function () {
   $("[data-toggle='popover']").popover({html: true, trigger: 'focus'});
+  $('.activities-text-block').equalizeHeights();
 });
