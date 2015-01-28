29a3c942ef65c731bcd6b424754ea8f2445a58fc
Beau Gunderson <beau@beaugunderson.com>
2015-01-28 11:05:50 -0800

    🌹 refactor quiz js (for real this time)

--- static/js/public-data-enroll-3-quiz.js
+++ static/js/public-data-enroll-3-quiz.js
@@ -1,10 +1,7 @@
-/*global $:true*/
-
 'use strict';
 
 var markdown = require('markdown/lib').markdown;
 var yaml = require('js-yaml');
-
 var $ = require('jquery');
 var _ = require('lodash');
 
@@ -34,17 +31,20 @@ function Form(container) {
   this.$formAccordion = $('#form-accordion');
 }
 
-function addQuestionsToHTML(form) {
+Form.prototype.addQuestionsToHtml = function () {
+  var self = this;
+
   var templateQuestion = _.template($('#question-template').html());
-  form.questions.forEach(function(question) {
-    form.$formAccordion.append(templateQuestion(question));
+
+  self.questions.forEach(function (question) {
+    self.$formAccordion.append(templateQuestion(question));
   });
 
   $('input').click(function () {
     var questionName = $(this).attr('name');
 
     // Get the question metadata
-    var question = _.find(form.questions, {name: questionName});
+    var question = _.find(self.questions, {name: questionName});
 
     // Get the question's containing div
     var $question = $('#' + questionName);
@@ -58,9 +58,9 @@ function addQuestionsToHTML(form) {
       $question.removeClass('correct-question');
     }
 
-    form.validate();
+    self.validate();
   });
-}
+};
 
 Form.prototype.setup = function () {
   var templateAnswer = _.template($('#answer-template').html());
@@ -68,44 +68,51 @@ Form.prototype.setup = function () {
   var self = this;
 
   $.get('/static/public-data/config/questions.yaml', function (questionsYaml) {
-    self.questions = yaml.safeLoad(questionsYaml).map(function (question) {
-      // Pre-parse sections that can contain Markdown
-      question.description = markdown.toHTML(question.description);
-      question.explanationText = markdown.toHTML(question.explanationText);
-      return question;
-    });
+    self.questions = yaml.safeLoad(questionsYaml)
+      .map(function (question) {
+        // Pre-parse sections that can contain Markdown
+        question.description = markdown.toHTML(question.description);
+        question.explanationText = markdown.toHTML(question.explanationText);
 
-    for (var i = 0; i < self.questions.length; i++) {
-      var question = self.questions[i];
-      question.isExpanded = 'false';
-      question.collapsedClass = 'collapsed';
-      question.inClass = '';
-      if (i === 0) {
-        question.isExpanded = 'true';
-        question.collapsedClass = '';
-        question.inClass = 'in';
-      }
-
-      // Parse and add answers to question
-      var answers = _(question.answers).map(function (answer, key) {
-        // This lets us use unquoted values for 'True' and 'False' in
-        // config/questions.yaml
-        if (answer === true) {
-          answer = 'True';
-        } else if (answer === false) {
-          answer = 'False';
+        return question;
+      })
+      .map(function (question, i) {
+        if (i === 0) {
+          question.isExpanded = 'true';
+          question.collapsedClass = '';
+          question.inClass = 'in';
+        } else {
+          question.isExpanded = 'false';
+          question.collapsedClass = 'collapsed';
+          question.inClass = '';
         }
-        return templateAnswer({
-          name: question.name,
-          key: key,
-          answer: markdownSnippet(answer)
-        });
-      }).join('\n');
-      question.answers = answers;
-    }
 
-    addQuestionsToHTML(self);
+        // Parse and add answers to question
+        var answers = _(question.answers).map(function (answer, key) {
+          // This lets us use unquoted values for 'True' and 'False' in
+          // config/questions.yaml
+          if (answer === true) {
+            answer = 'True';
+          } else if (answer === false) {
+            answer = 'False';
+          }
+
+          return templateAnswer({
+            name: question.name,
+            key: key,
+            answer: markdownSnippet(answer)
+          });
+        }).join('\n');
+
+        question.answers = answers;
+
+        return question;
+      });
+
+    self.addQuestionsToHtml();
   });
+
+  return this;
 };
 
 Form.prototype.validate = function () {
@@ -114,12 +121,9 @@ Form.prototype.validate = function () {
     return;
   }
 
-  console.log(this.$container);
-
   this.$container.addClass('complete');
 
   if (_.all(this.questions, questionPassed)) {
-
     if (this.$container.hasClass('failed')) {
       // User has been correcting answers. Draw attention back to the bottom of
       // the page by collapsing all questions.
@@ -130,7 +134,8 @@ Form.prototype.validate = function () {
       // The following works, but is uglier (visually, not just code).
       $(this.$container).find('.question-panel').each(function () {
         if ($(this).find('.panel-title a').not('collapsed').length) {
-          $(this).find('.panel-collapse, .collapse, .in').first().removeClass('in');
+          $(this).find('.panel-collapse, .collapse, .in')
+            .first().removeClass('in');
           $(this).find('.panel-title a').first().attr('aria-expanded', 'false');
           $(this).find('.panel-title a').first().addClass('collapsed');
         }
@@ -149,6 +154,5 @@ Form.prototype.validate = function () {
 };
 
 $(function () {
-  var form = new Form('#enrollment-quiz');
-  form.setup();
+  new Form('#enrollment-quiz').setup();
 });
