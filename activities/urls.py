d523b985554522fcd8509505a55b34af583798d0
Madeleine Ball <mpball@gmail.com>
2015-01-30 18:59:45 -0500

    🌹 Moving TaskUpdateView to data_import app

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import include, patterns, url
 
-from .views import TaskUpdateView
-
 from .twenty_three_and_me import urls as twenty_three_and_me_urls
 
 urlpatterns = patterns(
@@ -9,6 +7,4 @@ urlpatterns = patterns(
 
     # Activities
     url(r'^23andme/', include(twenty_three_and_me_urls, namespace='23andme')),
-
-    url(r'^task_update/', TaskUpdateView.as_view()),
 )
--- data_import/views.py
+++ data_import/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+
+from activities.twenty_three_and_me.models import DataRetrievalTask as \
+    DataRetrievalTask23andme
+
+
+class TaskUpdateView(View):
+    """Receive and record task success/failure input."""
+
+    task_retrieval_methods = {
+        'client.make_23andme_ohdataset':  DataRetrievalTask23andme.get_task,
+    }
+
+    def post(self, request):
+        task_name = request.POST['name']
+        task_state = request.POST['state']
+        s3_key_name = request.POST['s3_key_name']
+        response = self.update_task(task_name, task_state, s3_key_name)
+        return HttpResponse(response)
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(TaskUpdateView, self).dispatch(*args, **kwargs)
+
+    @classmethod
+    def update_task(cls, task_name, task_state, s3_key_name):
+        task_data = None
+        if task_name in cls.task_retrieval_methods:
+            task_data = cls.task_retrieval_methods[task_name](
+                filename=s3_key_name)
+        if not task_data:
+            return 'Invalid task and key name data!'
+        if task_state == 'SUCCESS':
+            task_data.status = task_data.TASK_SUCCESSFUL
+            task_data.complete_time = datetime.now()
+        elif task_state == 'FAILURE':
+            task_data.status = task_data.TASK_FAILED
+        task_data.save()
+        return 'Thanks!'
