be3f10debfa6a74ae9951402f40ffa98b334a444
Beau Gunderson <beau@beaugunderson.com>
2015-02-06 20:50:00 -0800

    🔧 implement the signal for American Gut barcodes

--- studies/american_gut/signals.py
+++ studies/american_gut/signals.py
@@ -1,7 +1,12 @@
+import json
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.contenttypes.models import ContentType
 
-from .models import Barcode
+from data_import.models import DataRetrievalTask
+
+from .models import Barcode, DataFile
 
 
 @receiver(post_save, sender=Barcode)
@@ -10,6 +15,17 @@ def post_save_cb(sender, instance, created, raw, update_fields, **kwargs):
     Initiate retrieval of the data corresponding to an American Gut barcode.
     """
     # If the model was created but not as part of a fixture
-    if created and not raw:
-        # Create a data retrieval task
-        pass
+    if raw or not created:
+        return
+
+    task = DataRetrievalTask(
+        datafile_model=ContentType.objects.get_for_model(DataFile),
+        user=instance.user_data.user,
+        app_task_params=json.dumps({'barcodes': [instance.value]}))
+
+    task.save()
+
+    if instance.user_data.user.member.primary_email.verified:
+        task.start_task()
+    else:
+        task.postpone_task()
