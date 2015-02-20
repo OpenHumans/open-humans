d7dc36412d25b87795226033b281508b2a218cf1
Madeleine Ball <mpball@gmail.com>
2015-02-19 20:51:22 -0500

    🔧  Study app verbose names that match the OAuth2 client app 'name'

--- studies/american_gut/apps.py
+++ studies/american_gut/apps.py
@@ -4,6 +4,11 @@ from django.apps import AppConfig
 class AmericanGutConfig(AppConfig):
     """
     Configure the American Gut study application.
+
+    Note: The verbose_name matches the 'name' assigned for this study's API
+    client. This is the 'name' field for its corresponding oauth2_provider
+    Application, and was set by the setup_api management command in
+    open_humans/management/commands/setup_api.py
     """
     name = 'studies.american_gut'
     verbose_name = 'American Gut'
---
+++ studies/go_viral/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class GoViralConfig(AppConfig):
+    """
+    Configure the GoViral study application.
+
+    Note: The verbose_name matches the 'name' assigned for this study's API
+    client. This is the 'name' field for its corresponding oauth2_provider
+    Application, and was set by the setup_api management command in
+    open_humans/management/commands/setup_api.py
+    """
+    name = 'studies.go_viral'
+    verbose_name = 'GoViral'
