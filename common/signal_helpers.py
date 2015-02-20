89704ca517c79f090bd32efe9f443783574a0fbb
Beau Gunderson <beau@beaugunderson.com>
2015-02-20 14:39:59 -0800

    🔧 add missing file

---
+++ common/signal_helpers.py
@@ -0,0 +1,27 @@
+import json
+
+from django.contrib.contenttypes.models import ContentType
+
+from data_import.models import DataRetrievalTask
+
+
+def task_signal(instance, created, raw, task_params, datafile_model):
+    """
+    A helper method that studies can use to create retrieval tasks when users
+    link datasets.
+    """
+    # If the model was created but not as part of a fixture
+    if raw or not created:
+        return
+
+    task = DataRetrievalTask(
+        datafile_model=ContentType.objects.get_for_model(datafile_model),
+        user=instance.user_data.user,
+        app_task_params=json.dumps(task_params))
+
+    task.save()
+
+    if instance.user_data.user.member.primary_email.verified:
+        task.start_task()
+    else:
+        task.postpone_task()
