d7addae3f0e85243b3067d66a991ea4e705acce0
Madeleine Ball <mpball@gmail.com>
2015-02-20 07:21:50 -0500

    🔧 Add verbose name for twenty_three_and_me to match oauth backend name

---
+++ activities/twenty_three_and_me/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+
+
+class TwentyThreeAndMeConfig(AppConfig):
+    """
+    Configure the 23andme activity application.
+
+    Note: The verbose_name matches the name of the 'provider' defined for this
+    activity's authentication backend, as used by python-social-auth. For this
+    activity, the backend is defined in common/oauth_backends.py
+    """
+    name = 'activities.twenty_three_and_me'
+    verbose_name = '23andme'
