1b956dc7da27d311df82832ef6af9b667452c0b6
Beau Gunderson <beau@beaugunderson.com>
2015-03-02 15:10:41 -0800

    🔧 update American Gut redirects

--- open_humans/middleware.py
+++ open_humans/middleware.py
@@ -5,7 +5,32 @@ from urlparse import urljoin
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
+from ipware.ip import get_ip
+
 logger = logging.getLogger(__name__)
+
+
+def get_production_redirect(request):
+    """
+    Generate an appropriate redirect to production.
+    """
+    redirect_url = urljoin(settings.PRODUCTION_URL, request.get_full_path())
+
+    logger.warning('Redirecting production client URL "%s" to "%s"',
+                   request.get_full_path(), redirect_url)
+
+    return HttpResponseRedirect(redirect_url)
+
+
+class RedirectAmericanGutToProductionMiddleware:
+    """
+    Redirect a request from American Gut to production.
+    """
+    def process_request(self, request):
+        if get_ip(request) != '128.138.93.14':
+            return
+
+        return get_production_redirect(request)
 
 
 class RedirectStagingToProductionMiddleware:
@@ -22,9 +47,4 @@ class RedirectStagingToProductionMiddleware:
         if request.GET['client_id'] not in settings.PRODUCTION_CLIENT_IDS:
             return
 
-        new_url = urljoin(settings.PRODUCTION_URL, request.get_full_path())
-
-        logger.warning('Redirecting production client URL "%s" to "%s"',
-                       request.get_full_path(), new_url)
-
-        return HttpResponseRedirect(new_url)
+        return get_production_redirect(request)
