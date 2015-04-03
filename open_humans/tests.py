e408091689be94952aab3bf0bf6306e842906fa4
Beau Gunderson <beau@beaugunderson.com>
2015-04-03 16:12:39 -0700

    ✅ add tests for our new user behaviors
    - test lookup by username/email
    - test case-insensitive unique constraint

--- open_humans/tests.py
+++ open_humans/tests.py
@@ -1,9 +1,13 @@
+from django.contrib import auth
+from django.db import IntegrityError
 from django.test import SimpleTestCase
 from django.test.utils import override_settings
 
 from oauth2_provider.models import AccessToken
 
 from common.testing import APITestCase
+
+UserModel = auth.get_user_model()
 
 
 class BasicAPITests(APITestCase):
@@ -73,3 +77,34 @@ class SmokeTests(SimpleTestCase):
             response = self.client.get(url)
             self.assertRedirects(response, '/account/login/?next={}'.format(
                 url))
+
+
+@override_settings(SSLIFY_DISABLE=True)
+class OpenHumansUserTests(SimpleTestCase):
+    """
+    Tests for our custom User class.
+    """
+
+    def setUp(self):  # noqa
+        try:
+            UserModel.objects.get(username='user1')
+        except UserModel.DoesNotExist:
+            UserModel.objects.create_user('user1', 'user1@test.com', 'user1')
+
+    def test_lookup_by_username(self):
+        user1 = auth.authenticate(username='user1', password='user1')
+
+        self.assertEqual(user1.username, 'user1')
+
+    def test_lookup_by_email(self):
+        user1 = auth.authenticate(username='user1@test.com', password='user1')
+
+        self.assertEqual(user1.username, 'user1')
+
+    def test_lowercase_unique(self):
+        # Create a lowercase user2
+        UserModel.objects.create_user('user2', 'user2@test.com', 'user2')
+
+        # Creating an uppercase USER2 should fail
+        self.assertRaises(IntegrityError, UserModel.objects.create_user,
+                          'USER2', 'other+user2@test.com', 'user2')
