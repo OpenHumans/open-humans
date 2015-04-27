3407b39157617dfbf867987563eda4ea895b23fc
Madeleine Ball <mpball@gmail.com>
2015-04-27 16:48:21 -0400

    🔧 Add password reset for Researchers

--- studies/urls_research.py
+++ studies/urls_research.py
@@ -7,8 +7,14 @@ from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static  # XXX: Best way to do this?
 from django.views.generic import TemplateView
 
-from account.views import (ConfirmEmailView as AccountConfirmEmailView,
-                           LogoutView as AccountLogoutView)
+from account.views import (
+    ConfirmEmailView as AccountConfirmEmailView,
+    LogoutView as AccountLogoutView,
+    PasswordResetView as AccountPasswordResetView,
+    PasswordResetTokenView as AccountPasswordResetTokenView)
+
+# Our custom form enforces minimum password length.
+from open_humans.forms import PasswordResetTokenForm
 
 from .views import (ResearcherLoginView, ResearcherSignupView,
                     ResearcherConfirmEmailView)
@@ -18,23 +24,32 @@ urlpatterns = patterns(
 
     url(r'^$', TemplateView.as_view(template_name='research/home.html'),
         name='home'),
+
+    # Account URLs.
     # Override to use custom form and view with added fields and methods.
     url(r'^account/signup/$', ResearcherSignupView.as_view(),
         name='account_signup'),
     url(r'^account/login/$', ResearcherLoginView.as_view(),
         name='account_login'),
-    # Can't override template wo/ overriding view for this one.
     url(r"^account/confirm_email/(?P<key>\w+)/$",
         ResearcherConfirmEmailView.as_view(),
         name="account_confirm_email"),
-    # Remaining account overrides: custom templates but using account views
+    # Remaining: account views w/custom templates/forms, when possible.
     url(r'^account/logout/$', AccountLogoutView.as_view(
         template_name='research/account/logout.html'), name='account_logout'),
-    url(r'^account/password/reset/$', TemplateView.as_view(
-        template_name='research/account/password_reset.html'),
+    url(r'^account/password/reset/$', AccountPasswordResetView.as_view(
+        template_name='research/account/password_reset.html',
+        template_name_sent='research/account/password_reset_sent.html'),
         name='account_password_reset'),
+    url(r"^account/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$",
+        AccountPasswordResetTokenView.as_view(
+            template_name='research/account/password_reset_token.html',
+            template_name_fail='research/account/password_reset_token_fail.html',
+            form_class=PasswordResetTokenForm
+        ), name="account_password_reset_token"),
     # Not actually part of accounts.
     url(r'^account/create/$',
         TemplateView.as_view(template_name='research/account/create.html'),
         name='account_create'),
+
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
