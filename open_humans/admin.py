7f3041ceb62ba18e6aba96ec3d341d7cadd6142e
Beau Gunderson <beau@beaugunderson.com>
2015-06-01 14:31:37 -0700

    🔧 Add models to admin site

--- open_humans/admin.py
+++ open_humans/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import Member, User
 
+admin.site.register(Member)
 admin.site.register(User, UserAdmin)
