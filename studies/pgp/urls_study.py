a3512ecdcfdcdfd6424017886feb6b2e48d8198d
Madeleine Ball <mpball@gmail.com>
2015-10-03 14:37:25 -0400

    🔧 Finish connection completion return page
    Closes #224.

--- studies/pgp/urls_study.py
+++ studies/pgp/urls_study.py
@@ -1,12 +1,9 @@
 from django.conf.urls import url
 
 from .views import DataRetrievalView
-from ..views import StudyConnectionReturnRedirectView
+
 
 urlpatterns = [
-    url(r'^return/$', StudyConnectionReturnRedirectView.as_view(),
-        name='study-connection-return'),
-
     url(r'^request-data-retrieval/$', DataRetrievalView.as_view(),
         name='request-data-retrieval'),
 ]
--- studies/urls_study.py
+++ studies/urls_study.py
@@ -7,12 +7,13 @@ from .american_gut import urls_study as american_gut_urls_study
 from .go_viral import urls_study as go_viral_urls_study
 from .pgp import urls_study as pgp_urls_study
 
-from .views import (StudyAuthorizationView, StudyConnectionView,
-                    StudyCompletionView)
+from .views import (StudyAuthorizationView, StudyConnectionReturnView,
+                    StudyConnectionView, StudyCompletionView)
 
 urlpatterns = patterns(
     '',
 
+    url(r'^(?P<name>[a-z_-]*)/return/$', StudyConnectionReturnView.as_view()),
     url(r'^american-gut/',
         include(american_gut_urls_study, namespace='american-gut')),
     url(r'^go-viral/', include(go_viral_urls_study, namespace='go-viral')),
