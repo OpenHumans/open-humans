504bb6393dee477a3c9bcaf45d0aa5b1f8dadb12
Madeleine Ball <mpball@gmail.com>
2015-10-04 22:46:11 -0400

    ✅ Add simple tests for connection return pages

--- studies/american_gut/tests.py
+++ studies/american_gut/tests.py
@@ -1,3 +1,4 @@
+from django.test import SimpleTestCase
 from oauth2_provider.models import AccessToken
 
 from common.testing import APITestCase
@@ -40,3 +41,18 @@ class UserDataTests(APITestCase):
         self.verify_request('/barcodes/111111/', status=401)
         self.verify_request('/barcodes/111111/', status=401, method='delete')
         self.verify_request('/barcodes/555555/', status=401)
+
+
+class StudyTests(SimpleTestCase):
+
+    def test_connection_return(self):
+        response = self.client.get('/study/american_gut/return/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(
+            '/study/american_gut/return/?origin=open-humans')
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get('/study/american-gut/return/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(
+            '/study/american-gut/return/?origin=open-humans')
+        self.assertEqual(response.status_code, 302)
