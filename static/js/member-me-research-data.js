14a20a2548cac8c538982a85725aa20b9351dda0
Beau Gunderson <beau@beaugunderson.com>
2015-11-09 17:18:16 -0800

    🔧 implement AJAX sharing toggle for #287

--- static/js/member-me-research-data.js
+++ static/js/member-me-research-data.js
@@ -6,4 +6,22 @@ require('bootstrap');
 
 $(function () {
   $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
+
+  // AJAX toggling for public data sharing
+  $('form').on('click', 'input[type=submit]', function (e) {
+    e.preventDefault();
+
+    var isPublic = $(this).siblings('input[name=public]').val() === 'True';
+    var formUrl = $(this).parent().attr('action');
+
+    var newState = isPublic ? 'False' : 'True';
+    var newValue = isPublic ? 'Stop public sharing' : 'Share publicly';
+
+    var self = this;
+
+    $.post(formUrl, $(this).parent().serialize(), function () {
+      $(self).val(newValue);
+      $(self).siblings('input[name=public]').val(newState);
+    });
+  });
 });
