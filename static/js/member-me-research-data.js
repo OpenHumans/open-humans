90e79c48cad65dc77c0680d61a47f9c680724a9a
Beau Gunderson <beau@beaugunderson.com>
2015-11-10 16:53:14 -0800

    🔧 add error-handling for AJAX sharing toggle

--- static/js/member-me-research-data.js
+++ static/js/member-me-research-data.js
@@ -11,17 +11,22 @@ $(function () {
   $('form').on('click', 'input[type=submit]', function (e) {
     e.preventDefault();
 
+    var $form = $(this).parent();
+    var formUrl = $form.attr('action');
+
     var isPublic = $(this).siblings('input[name=public]').val() === 'True';
-    var formUrl = $(this).parent().attr('action');
 
     var newState = isPublic ? 'False' : 'True';
     var newValue = isPublic ? 'Stop public sharing' : 'Share publicly';
 
     var self = this;
 
-    $.post(formUrl, $(this).parent().serialize(), function () {
+    $.post(formUrl, $form.serialize(), function () {
       $(self).val(newValue);
       $(self).siblings('input[name=public]').val(newState);
+    }).fail(function () {
+      // fall back to a regular form submission if AJAX doesn't work
+      $form.submit();
     });
   });
 });
