4d172a5ccccc972be49e78eac9695d6427bb4557
Beau Gunderson <beau@beaugunderson.com>
2016-01-04 18:47:16 -0800

    👕 fix long lines

--- open_humans/views.py
+++ open_humans/views.py
@@ -69,7 +69,8 @@ class MemberDetailView(DetailView):
         context.update({
             'next': reverse_lazy('member-detail',
                                  kwargs={'slug': self.object.user.username}),
-            'public_data_tasks': self.object.public_data_participant.public_data_tasks,
+            'public_data_tasks':
+                self.object.public_data_participant.public_data_tasks,
         })
 
         return context
@@ -138,8 +139,9 @@ class MyMemberDashboardView(PrivateMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super(MyMemberDashboardView, self).get_context_data(**kwargs)
         context.update({
-            'public_data_tasks':
-                self.object.user.member.public_data_participant.public_data_tasks,
+            'public_data_tasks': (self.object.user.member
+                                  .public_data_participant
+                                  .public_data_tasks),
         })
 
         return context
