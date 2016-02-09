4d114f645af481127a9acefa8c58df89e5623784
Beau Gunderson <beau@beaugunderson.com>
2016-02-09 12:38:57 -0800

    🔧 require a file upload before continuing
    fixes #390

--- static/js/activities-data-selfie-upload.js
+++ static/js/activities-data-selfie-upload.js
@@ -1,5 +1,23 @@
 'use strict';
 
 var cancelNavigation = require('./lib/cancel-navigation.js');
+var $ = require('jquery');
 
 cancelNavigation('#go-to-data-selfie');
+
+window.dropzoneOptions = {
+  addRemoveLinks: true,
+
+  customInit: function () {
+    this.on('success', function () {
+      $('#upload-file-first').hide();
+      $('#go-to-data-selfie').show();
+    });
+  }
+};
+
+$(function () {
+  $('#upload-file-first').click(function (e) {
+    e.preventDefault();
+  });
+});
