c95edd812ada05dae045b15f0ec854631b31cd38
Beau Gunderson <beau@beaugunderson.com>
2016-02-10 23:55:03 -0800

    🔧 add helper for public data API

--- data_import/models.py
+++ data_import/models.py
@@ -2,7 +2,7 @@ import json
 import os
 import urlparse
 
-from collections import OrderedDict
+from collections import defaultdict, OrderedDict
 from itertools import groupby
 from operator import attrgetter
 
@@ -11,6 +11,7 @@ import requests
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models import F
 from django.dispatch import receiver
 from django.utils import timezone
 
@@ -236,6 +237,28 @@ class DataFileManager(models.Manager):
         super(DataFileManager, self).contribute_to_class(model, name)
 
         models.signals.pre_delete.connect(delete_file, model)
+
+    def public(self):
+        data_files = (
+            self
+            .filter(
+                user__member__public_data_participant__publicdataaccess__is_public=True,
+                user__member__public_data_participant__publicdataaccess__data_source=F('source'))
+            .order_by('user__username', '-task__start_time'))
+
+        tasks = defaultdict(dict)
+        results = []
+
+        for data_file in data_files:
+            if data_file.user_id not in tasks[data_file.source]:
+                tasks[data_file.source][data_file.user_id] = data_file.task_id
+
+            if tasks[data_file.source][data_file.user_id] != data_file.task_id:
+                continue
+
+            results.append(data_file)
+
+        return results
 
 
 class DataFile(models.Model):
