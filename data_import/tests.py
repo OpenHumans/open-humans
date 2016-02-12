d9e677f1661b0fadae1f8e09de0a742ce53f1ae4
Beau Gunderson <beau@beaugunderson.com>
2016-02-12 13:50:25 -0800

    ✅ add some data_import tests

--- data_import/tests.py
+++ data_import/tests.py
@@ -13,6 +13,19 @@ from .models import DataRetrievalTask, DataFile
 UserModel = auth.get_user_model()
 
 
+def create_user(name):
+    """
+    Helper to create a Django user.
+    """
+    try:
+        user = UserModel.objects.get(username=name)
+    except UserModel.DoesNotExist:
+        user = UserModel.objects.create_user(
+            name, '{}@test.com'.format(name), name)
+
+    return user
+
+
 @override_settings(SSLIFY_DISABLE=True)
 class TaskUpdateTests(TestCase):
     """
@@ -23,18 +36,30 @@ class TaskUpdateTests(TestCase):
     def setUpClass(cls):
         super(TaskUpdateTests, cls).setUpClass()
 
-        try:
-            user = UserModel.objects.get(username='user1')
-        except UserModel.DoesNotExist:
-            user = UserModel.objects.create_user('user1', 'user1@test.com',
-                                                 'user1')
-        Member.objects.get_or_create(user=user)
+        user1 = create_user('user1')
+        user2 = create_user('user2')
 
-        cls.task1 = DataRetrievalTask(user=user, source='pgp')
-        cls.task2 = DataRetrievalTask(user=user, source='pgp')
+        Member.objects.get_or_create(user=user1)
+
+        # test both verified and unverified users
+        (member2, _) = Member.objects.get_or_create(user=user2)
+        email = member2.primary_email
+
+        email.verified = True
+        email.save()
+
+        cls.task1 = DataRetrievalTask(user=user1, source='pgp')
+        cls.task2 = DataRetrievalTask(user=user1, source='pgp')
+
+        cls.task3 = DataRetrievalTask(user=user2, source='pgp')
+        cls.task4 = DataRetrievalTask(user=user2, source='pgp')
+        cls.task5 = DataRetrievalTask(user=user2, source='go_viral')
 
         cls.task1.save()
         cls.task2.save()
+        cls.task3.save()
+        cls.task4.save()
+        cls.task5.save()
 
     def test_for_user(self):
         user = UserModel.objects.get(username='user1')
@@ -42,12 +67,28 @@ class TaskUpdateTests(TestCase):
 
         self.assertEqual(len(tasks), 2)
 
+    def test_for_user_verified(self):
+        user = UserModel.objects.get(username='user2')
+        tasks = DataRetrievalTask.objects.for_user(user)
+
+        self.assertEqual(len(tasks), 3)
+
     def test_grouped_recent(self):
         user = UserModel.objects.get(username='user1')
         tasks = DataRetrievalTask.objects.for_user(user)
         grouped_recent = tasks.grouped_recent()
 
         self.assertEqual(grouped_recent, {'pgp': self.task2})
+
+    def test_grouped_recent_verified(self):
+        user = UserModel.objects.get(username='user2')
+        tasks = DataRetrievalTask.objects.for_user(user)
+        grouped_recent = tasks.grouped_recent()
+
+        self.assertEqual(grouped_recent, {
+            'pgp': self.task4,
+            'go_viral': self.task5
+        })
 
     def test_task_update_create_datafiles(self):
         data = {
@@ -143,8 +184,34 @@ class TaskUpdateTests(TestCase):
 
             self.assertEqual(task.status, choice)
 
+    def test_task_update_task_state_no_task_data(self):
+        response = self.client.post(reverse('data-import:task-update'), {})
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_task_update_task_state_bad_task_id(self):
+        data = {
+            'task_data': json.dumps({
+                'task_id': -1,
+            })
+        }
+
+        response = self.client.post(reverse('data-import:task-update'), data)
+
+        # TODO: change to 400?
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, 'Invalid task ID!')
+
     def test_data_retrieval_view(self):
         login = self.client.login(username='user1', password='user1')
+        self.assertEqual(login, True)
+
+        response = self.client.post(
+            reverse('studies:pgp:request-data-retrieval'))
+        self.assertRedirects(response, reverse('my-member-research-data'))
+
+    def test_data_retrieval_view_verified(self):
+        login = self.client.login(username='user2', password='user2')
         self.assertEqual(login, True)
 
         response = self.client.post(
