89a244c6f33d719814eb1ce93a2e48fef677688e
Beau Gunderson <beau@beaugunderson.com>
2016-02-26 22:51:51 -0800

    🔧 add Moves API

---
+++ activities/moves/apps.py
@@ -0,0 +1,14 @@
+from activities.app_configs import UserSocialAuthAppConfig
+
+
+class MovesConfig(UserSocialAuthAppConfig):
+    """
+    Configure the Moves activity application.
+    """
+    name = __package__
+    verbose_name = 'Moves'
+
+    data_description = {
+        'name': '',
+        'description': '',
+    }
