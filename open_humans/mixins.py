152dbe658908f101223f213d247ced74d647f53c
Beau Gunderson <beau@beaugunderson.com>
2016-02-28 18:21:27 -0800

    fix bug in SourcesContextMixin

--- open_humans/mixins.py
+++ open_humans/mixins.py
@@ -9,7 +9,9 @@ class SourcesContextMixin(object):
     def get_context_data(self, **kwargs):
         context = super(SourcesContextMixin, self).get_context_data(**kwargs)
 
-        context.update({label: app_label_to_user_data_model(label)
-                        for label in get_source_labels()})
+        context.update({
+            'sources': {label: app_label_to_user_data_model(label)
+                        for label in get_source_labels()}
+        })
 
         return context
