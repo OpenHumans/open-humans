0fe64b89b146db3a4bbe726bddec71093a58b05b
Beau Gunderson <beau@beaugunderson.com>
2016-03-01 14:50:14 -0800

    🔧 add data description for Moves

--- activities/moves/apps.py
+++ activities/moves/apps.py
@@ -12,6 +12,8 @@ class MovesConfig(UserSocialAuthAppConfig):
     in_development = True
 
     data_description = {
-        'name': '',
-        'description': '',
+        'name': 'Location and GPS data',
+        'description': """Your Moves data contains locations and GPS recordings
+        of your movements from everywhere you've been while the app has been
+        running on your device.""",
     }
