dfffd88d26afb477bff042b23356fdbc26da8c66
Beau Gunderson <beau@beaugunderson.com>
2016-02-29 22:01:02 -0800

    🔧 add in_development flag

--- activities/fitbit/apps.py
+++ activities/fitbit/apps.py
@@ -5,8 +5,11 @@ class FitbitConfig(UserSocialAuthAppConfig):
     """
     Configure the Fitbit activity application.
     """
+
     name = __package__
     verbose_name = 'Fitbit'
+
+    in_development = True
 
     data_description = {
         'name': 'Health and activity data',
