6c3b9b9d43a3bc597aa8d73cd168ac7bc2c64972
Beau Gunderson <beau@beaugunderson.com>
2016-03-01 14:43:20 -0800

    🔥 remove unused 23andMe OAuth2 backend

--- open_humans/settings.py
+++ open_humans/settings.py
@@ -451,7 +451,6 @@ REST_FRAMEWORK = {
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'social.backends.runkeeper.RunKeeperOAuth2',
-    'common.oauth_backends.TwentyThreeAndMeOAuth2',
 )
 
 GO_VIRAL_MANAGEMENT_TOKEN = os.getenv('GO_VIRAL_MANAGEMENT_TOKEN')
