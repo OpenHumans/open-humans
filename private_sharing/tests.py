c3eca42da1197096d5ae8d1f2f8a99da71ab8fd0
Beau Gunderson <beau@beaugunderson.com>
2016-03-12 15:51:57 -0800

    ✅ add test to create an on-site project

--- private_sharing/tests.py
+++ private_sharing/tests.py
@@ -29,7 +29,8 @@ class PrivateSharingTests(TestCase):
 
         user1 = get_or_create_user('user1')
         cls.member1, _ = Member.objects.get_or_create(user=user1)
-        cls.member1_project = OnSiteDataRequestProject.objects.get(slug='abc-2')
+        cls.member1_project = OnSiteDataRequestProject.objects.get(
+            slug='abc-2')
         email1 = cls.member1.primary_email
 
         email1.verified = True
@@ -182,3 +183,77 @@ class BrowserTests(BrowserTestCase):
             ('You have successfully joined the project "abc 2".' in
              driver.find_element_by_css_selector('.message.success').text),
             True)
+
+    def test_create_on_site(self):
+        driver = self.driver
+
+        self.login()
+
+        driver.get(self.live_server_url + '/direct-sharing/projects/manage/')
+
+        driver.find_element_by_link_text(
+            'Create a new on-site data request project').click()
+
+        driver.find_element_by_id('id_is_study_1').click()
+
+        driver.find_element_by_css_selector('div.radio > label').click()
+
+        driver.find_element_by_id('id_name').clear()
+        driver.find_element_by_id('id_name').send_keys('Test Study')
+
+        driver.find_element_by_id('id_leader').clear()
+        driver.find_element_by_id('id_leader').send_keys('Beau Gunderson')
+
+        driver.find_element_by_id('id_organization').clear()
+        driver.find_element_by_id('id_organization').send_keys('N/A')
+
+        driver.find_element_by_id('id_contact_email').clear()
+        driver.find_element_by_id('id_contact_email').send_keys(
+            'beau@beaugunderson.com')
+
+        driver.find_element_by_id('id_info_url').clear()
+        driver.find_element_by_id('id_info_url').send_keys(
+            'https://beaugunderson.com/')
+
+        driver.find_element_by_id('id_short_description').clear()
+        driver.find_element_by_id('id_short_description').send_keys(
+            'Just testing!')
+
+        driver.find_element_by_id('id_long_description').clear()
+        driver.find_element_by_id('id_long_description').send_keys(
+            'Just testing!')
+
+        driver.find_element_by_id('id_badge_image').clear()
+        driver.find_element_by_id('id_badge_image').send_keys(
+            '/Users/beau/Documents/TwitterHeader.jpg')
+
+        driver.find_element_by_id('id_request_sources_access_1').click()
+        driver.find_element_by_id('id_request_sources_access_2').click()
+        driver.find_element_by_id('id_request_sources_access_3').click()
+        driver.find_element_by_id('id_request_sources_access_4').click()
+        driver.find_element_by_id('id_request_sources_access_5').click()
+        driver.find_element_by_id('id_request_sources_access_6').click()
+        driver.find_element_by_id('id_request_sources_access_7').click()
+        driver.find_element_by_id('id_request_sources_access_8').click()
+        driver.find_element_by_id('id_request_sources_access_9').click()
+        driver.find_element_by_id('id_request_sources_access_10').click()
+        driver.find_element_by_id('id_request_sources_access_11').click()
+        driver.find_element_by_id('id_request_sources_access_12').click()
+        driver.find_element_by_id('id_request_sources_access_13').click()
+
+        driver.find_element_by_id('id_request_message_permission_1').click()
+
+        driver.find_element_by_id('id_request_username_access_1').click()
+
+        driver.find_element_by_id('id_consent_text').clear()
+        driver.find_element_by_id('id_consent_text').send_keys(
+            '## Consent form\n\n- list item 1\n- list item 2\n- list item 3')
+
+        driver.find_element_by_id('id_post_sharing_url').clear()
+        driver.find_element_by_id('id_post_sharing_url').send_keys(
+            'https://beaugunderson.com/?id=OH_PROJECT_MEMBER_ID')
+
+        driver.find_element_by_id('create-project').click()
+
+        self.assertEqual('Test Study', driver.find_element_by_xpath(
+            "//table[@id='on-site-projects']/tbody/tr[1]/td").text)
