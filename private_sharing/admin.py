5e3088b573846c69a157e0505540318c8880dfef
Beau Gunderson <beau@beaugunderson.com>
2016-03-22 16:18:02 -0700

    🔧 add created field to admin

--- private_sharing/admin.py
+++ private_sharing/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from . import models
 
+
+class DataRequestProjectMemberAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
 admin.site.register(models.DataRequestProject)
 admin.site.register(models.OAuth2DataRequestProject)
 admin.site.register(models.OnSiteDataRequestProject)
-admin.site.register(models.DataRequestProjectMember)
+admin.site.register(models.DataRequestProjectMember,
+                    DataRequestProjectMemberAdmin)
