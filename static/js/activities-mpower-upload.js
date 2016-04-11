503ff0724e342d9135e2940293d80817111f52c1
Beau Gunderson <beau@beaugunderson.com>
2016-04-11 09:15:47 -0700

    🔧 (actually implement the previous commit)

--- static/js/activities-mpower-upload.js
+++ static/js/activities-mpower-upload.js
@@ -1,5 +1,21 @@
 'use strict';
 
 var cancelNavigation = require('./lib/cancel-navigation.js');
+var $ = require('jquery');
 
 cancelNavigation('#process-file');
+
+window.dropzoneOptions = {
+  customInit: function () {
+    this.on('success', function () {
+      $('#upload-file-first').hide();
+      $('#process-file').show();
+    });
+  }
+};
+
+$(function () {
+  $('#upload-file-first').click(function (e) {
+    e.preventDefault();
+  });
+});
