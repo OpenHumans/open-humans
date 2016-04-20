b222f9916d8c689a8310c2e885954822ea8378e7
Beau Gunderson <beau@beaugunderson.com>
2016-04-20 07:50:55 -0700

    🔧 release the new activities grid

--- static/js/activities.js
+++ static/js/activities.js
@@ -4,16 +4,41 @@ var $ = window.jQuery = require('jquery');
 
 require('bootstrap');
 
-// From example code here: http://api.jquery.com/map/
-$.fn.equalizeHeights = function () {
-  var maxHeight = this.map(function (i, e) {
-    return $(e).height();
-  }).get();
-
-  return this.height(Math.max.apply(this, maxHeight));
-};
-
 $(function () {
   $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
-  $('.activities-text-block').equalizeHeights();
+
+  $('.grid').imagesLoaded(function () {
+    var $grid = $('.grid').isotope({
+      itemSelector: '.item',
+      layoutMode: 'masonry',
+      masonry: {
+        columnWidth: '.col-md-4',
+        percentPosition: true
+      }
+    });
+
+    var filter;
+
+    $grid.on('arrangeComplete', function (event, filteredItems) {
+      if (!filteredItems.length) {
+        $('.filter-empty[data-filter="' + filter + '"').show();
+      }
+    });
+
+    $('.filters button').click(function () {
+      $('.filters button').removeClass('selected');
+      $(this).addClass('selected');
+
+      filter = $(this).attr('data-filter');
+
+      $('.filter-empty').hide();
+
+      $('.filter-description').hide();
+      $('.filter-description[data-filter="' + filter + '"').show();
+
+      $grid.isotope({filter: filter});
+    });
+
+    $('.filters button[data-filter="*"]').click();
+  });
 });
