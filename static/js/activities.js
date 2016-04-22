4f5317dcde761fd321798e4fef8255ed4a14d386
Beau Gunderson <beau@beaugunderson.com>
2016-04-22 12:14:37 -0700

    🐎🔧 add linkable filters; remove loading delay

--- static/js/activities.js
+++ static/js/activities.js
@@ -4,42 +4,56 @@ var $ = window.jQuery = require('jquery');
 
 require('bootstrap');
 
+function getHashFilter() {
+  var matches = location.hash.match(/filter=([^&]+)/i);
+  var filter = matches && matches[1];
+
+  return (filter && decodeURIComponent(filter)) || '*';
+}
+
 $(function () {
   $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
 
-  $('.grid').imagesLoaded(function () {
-    var $grid = $('.grid').isotope({
-      itemSelector: '.item',
-      layoutMode: 'masonry',
-      masonry: {
-        columnWidth: '.col-md-4',
-        percentPosition: true
-      }
-    });
-
-    var filter;
-
-    $grid.on('arrangeComplete', function (event, filteredItems) {
-      if (!filteredItems.length) {
-        $('.filter-description').hide();
-        $('.filter-empty[data-filter="' + filter + '"]').show();
-      }
-    });
-
-    $('.filters button').click(function () {
-      $('.filters button').removeClass('selected');
-      $(this).addClass('selected');
-
-      filter = $(this).attr('data-filter');
-
-      $('.filter-empty').hide();
-
-      $('.filter-description').hide();
-      $('.filter-description[data-filter="' + filter + '"]').show();
-
-      $grid.isotope({filter: filter});
-    });
-
-    $('.filters button[data-filter="*"]').click();
+  var $grid = $('.grid').isotope({
+    itemSelector: '.item',
+    layoutMode: 'masonry',
+    masonry: {
+      columnWidth: '.col-md-4',
+      percentPosition: true
+    }
   });
+
+  $('.grid').imagesLoaded(function () {
+    $('.grid').isotope('layout');
+  });
+
+  function onHashchange() {
+    var filter = getHashFilter();
+
+    $('.filters button').removeClass('selected');
+    $('.filters button[data-filter="' + filter + '"]').addClass('selected');
+
+    $('.filter-empty').hide();
+
+    $('.filter-description').hide();
+    $('.filter-description[data-filter="' + filter + '"]').show();
+
+    $grid.isotope({filter: filter});
+
+    var filteredItems = $('.grid ' + filter);
+
+    if (!filteredItems.length) {
+      $('.filter-description').hide();
+      $('.filter-empty[data-filter="' + getHashFilter() + '"]').show();
+    }
+  }
+
+  $('.filters button').click(function () {
+    location.hash = 'filter=' +
+      encodeURIComponent($(this).attr('data-filter'));
+  });
+
+  $(window).on('hashchange', onHashchange);
+
+  onHashchange();
 });
