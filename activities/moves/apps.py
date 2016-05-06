e608413efd3184a38c4cac33a2fdc03477d2cef5
Madeleine Ball <mpball@gmail.com>
2016-05-06 09:14:33 -0400

    ✒ Improve Moves description

--- activities/moves/apps.py
+++ activities/moves/apps.py
@@ -9,13 +9,13 @@ class MovesConfig(UserSocialAuthAppConfig):
     name = __package__
     verbose_name = 'Moves'
 
-    description = """Moves is an always-on location logging app for iPhone and
-    Android. It classifies activities as walking, cycling, running, transit,
-    and many other types."""
+    description = """Moves is an always-on steps and location logging app for
+    iPhone and Android. It classifies activities as walking, cycling, running,
+    transit, and many other types."""
 
     data_description = {
         'name': 'Location and GPS data',
-        'description': """Your Moves data contains locations and GPS recordings
-        of your movements from everywhere you've been while the app has been
-        running on your device.""",
+        'description': """Your Moves data contains step counts, locations, and
+        GPS recordings of your movements from everywhere you've been while the
+        app has been running on your device.""",
     }
