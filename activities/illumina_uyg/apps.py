c44d3f1f1aa5e78dd7dfff6a03c8cf5e02251047
Beau Gunderson <beau@beaugunderson.com>
2016-06-13 10:03:38 -0700

    🔧 move Illumina UYG back to development
    (we'll leave it in development until the upload-side and server-side
    validation are implemented)

--- activities/illumina_uyg/apps.py
+++ activities/illumina_uyg/apps.py
@@ -13,7 +13,7 @@ class IlluminaUYGConfig(UploadAppConfig):
 
     url_slug = 'illumina-uyg'
 
-    in_development = False
+    in_development = True
 
     href_connect = reverse_lazy('activities:illumina-uyg:upload')
     href_add_data = reverse_lazy('activities:illumina-uyg:upload')
