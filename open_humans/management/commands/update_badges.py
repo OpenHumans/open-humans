b07e522f569b6ef826b946a34aa7dfe4418e43bf
Beau Gunderson <beau@beaugunderson.com>
2016-08-29 21:03:47 -0700

    🔧 move update_badges to management command

--- open_humans/management/commands/update_badges.py
+++ open_humans/management/commands/update_badges.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 
+from common.activities import personalize_activities_dict
+
 from open_humans.models import Member
 
 
@@ -13,9 +15,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('Updating badges')
 
+        badge_data = {k: v['badge']
+                      for k, v in personalize_activities_dict().items()}
+
         for member in Member.enriched.all():
             self.stdout.write('- {0}'.format(member.user.username))
 
-            member.update_badges()
+            member.badges = []
+
+            # Badges for activities and deeply integrated studies, e.g. PGP,
+            # RunKeeper
+            for label, _ in member.connections.items():
+                member.badges.append(badge_data[label])
+
+            project_memberships = member.datarequestprojectmember_set.filter(
+                project__approved=True,
+                joined=True,
+                authorized=True,
+                revoked=False)
+
+            # Badges for DataRequestProjects
+            for membership in project_memberships:
+                member.badges.append(badge_data[membership.project.id_label])
+
+            # The badge for the Public Data Sharing Study
+            if member.public_data_participant.enrolled:
+                member.badges.append(badge_data['public_data_sharing'])
+
+            member.save()
 
         self.stdout.write('Done')
