ad918bfe1ec464d96b2679f0ce3cf5eea4533a7c
Beau Gunderson <beau@beaugunderson.com>
2016-09-20 10:11:48 -0700

    🔧 add missing JS file

---
+++ static/js/activity-management.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var $ = window.jQuery = require('jquery');
+var publicSharingToggle = require('./lib/public-sharing-toggle.js');
+
+require('bootstrap');
+
+$(function () {
+  $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
+
+  publicSharingToggle();
+
+  $('form.delete-selfie-file').on('click', 'input[type=submit]', function (e) {
+    e.preventDefault();
+
+    var $form = $(this).parent();
+    var formUrl = $form.attr('action');
+
+    var self = this;
+
+    $.post(formUrl, $form.serialize(), function () {
+      var $tr = $(self).parents('tr').eq(0);
+
+      $tr.hide('slow', function () {
+        $tr.remove();
+      });
+    }).fail(function () {
+      // fall back to a regular form submission if AJAX doesn't work
+      $form.submit();
+    });
+  });
+});
