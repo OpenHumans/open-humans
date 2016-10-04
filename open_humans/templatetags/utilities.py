c0be3b5828ab05407300a6e8a369409b4b4ab5b5
Madeleine Ball <mpball@gmail.com>
2016-10-04 15:30:23 -0400

    📐 improve layout for project authorization
    Add links to the activity pages for all requested sources.
    
    Fix the "double button" showing up when a requested source is
    characterized as both "data source" and "share data".

--- open_humans/templatetags/utilities.py
+++ open_humans/templatetags/utilities.py
@@ -6,6 +6,7 @@ import bleach
 import markdown as markdown_library
 
 from django import template
+from django.apps import apps
 from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.core.urlresolvers import reverse, NoReverseMatch
@@ -14,7 +15,8 @@ from django.utils.safestring import mark_safe
 
 from common.activities import personalize_activities_dict
 from common.utils import full_url as full_url_method
-from private_sharing.models import app_label_to_verbose_name_including_dynamic
+from private_sharing.models import (app_label_to_verbose_name_including_dynamic,
+                                    DataRequestProject)
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +32,31 @@ def source_to_name(source):
         return app_label_to_verbose_name_including_dynamic(source)
     except:  # pylint: disable=bare-except
         return source
+
+
+@register.filter
+def source_to_url_slug(source):
+    """
+    Return url_slug for an "app" activity, or slug for "project" activity.
+
+    "App" activities refers to activities that are apps within Open Humans,
+    e.g. activities/fitbit, activities/ancestry_dna, and studies/american_gut.
+
+    "Project" activities refers to standard on-site and OAuth2 projects
+    (i.e. the DataRequestProject model).
+
+    Returned slug should be valid input for the 'activity-management' page.
+    """
+    try:
+        return apps.get_app_config(source).url_slug
+    except AttributeError:
+        return source
+    except LookupError:
+        match = re.match(r'direct-sharing-(?P<id>\d+)', source)
+
+        if match:
+            project = DataRequestProject.objects.get(id=int(match.group('id')))
+            return project.slug
 
 
 @register.filter
