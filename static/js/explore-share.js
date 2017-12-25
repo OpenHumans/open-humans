2a9ae04a5d957a481cbcf0efbbb25751398ea21f
Madeleine Price Ball <mpball@gmail.com>
2017-12-25 15:17:54 -0800

    New pages and updated navbar

---
+++ static/js/explore-share.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var $ = window.jQuery;
+
+require('bootstrap');
+
+$(function () {
+  $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
+
+  var $grid = $('.grid').isotope({
+    itemSelector: '.item',
+    layoutMode: 'masonry',
+    masonry: {
+      columnWidth: '.col-md-4',
+      percentPosition: true
+    }
+  });
+
+  $('.grid').imagesLoaded(function () {
+    $('.grid').isotope('layout');
+  });
+
+});
