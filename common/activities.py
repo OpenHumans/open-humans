471d3ead065378f42b16d9ef0bf15871b7357717
Mad Price Ball <mpball@gmail.com>
2018-04-09 13:34:41 -0700

    Move legacy VCF data (genome/exome) to new project

--- common/activities.py
+++ common/activities.py
@@ -326,9 +326,6 @@ def manual_overrides(user, activities):
     activities['data_selfie']['manage_files'] = reverse(
         'activities:data-selfie:manage')
 
-    activities['vcf_data']['manage_files'] = reverse(
-        'activities:genome-exome-data:manage-files')
-
     activities['ubiome']['manage_files'] = reverse(
         'activities:ubiome:manage-samples')
 
