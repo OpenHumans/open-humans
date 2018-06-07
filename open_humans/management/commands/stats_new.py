fd6e17866a3eb31895469c215e5d893628de96fe
Mad Price Ball <mpball@gmail.com>
2018-06-07 12:39:26 -0700

    Update stats script

--- open_humans/management/commands/stats_new.py
+++ open_humans/management/commands/stats_new.py
@@ -19,39 +19,52 @@ class Command(BaseCommand):
     args = ''
 
     def add_arguments(self, parser):
-        parser.add_argument('--date', type=str,
+        parser.add_argument('--start-date', type=str,
                             help='stats as of this date')
+        parser.add_argument('--end-date', type=str,
+                            help='stats up to this date')
 
     def handle(self, *args, **options):
-        cutoff = arrow.get(options['date']).datetime
+        start_cutoff = arrow.get(options['start_date'])
+        end_cutoff = arrow.get(options['end_date'])
 
-        members = Member.objects.filter(
-            user__date_joined__lte=cutoff).filter(
-            user__is_active=True)
-        members_with_data = members.annotate(
-            datafiles_count=Count('user__datafiles')).filter(
-            datafiles_count__gte=1)
-        files = DataFile.objects.exclude(archived__lte=cutoff).filter(
-            created__lte=cutoff)
-        projects_made = DataRequestProject.objects.filter(created__lte=cutoff)
-        projects_approved = projects_made.filter(approved=True)
+        for date in arrow.Arrow.range('day', start_cutoff, end_cutoff):
+            cutoff = date.datetime
+            members = Member.objects.filter(
+                user__date_joined__lte=cutoff).filter(
+                user__is_active=True)
+            members_with_data = members.annotate(
+                datafiles_count=Count('user__datafiles')).filter(
+                datafiles_count__gte=1)
+            projects_made = DataRequestProject.objects.filter(created__lte=cutoff)
+            projects_approved = projects_made.filter(approved=True)
 
-        data_connections = set([(
-            df['source'], df['user__username']) for
-            df in DataFile.objects.exclude(
-            archived__lte=cutoff).filter(
-            user__is_active=True).filter(
-            created__lte=cutoff).values('user__username', 'source')])
+            data_connections = DataRequestProjectMember.objects.exclude(
+                project__approved=False).exclude(joined=False).filter(
+                member__user__is_active=True).exclude(authorized=False).exclude(
+                project__returned_data_description='').filter(created__lte=cutoff)
 
-        proj_connections = DataRequestProjectMember.objects.exclude(
-            project__approved=False).exclude(joined=False).filter(
-            member__user__is_active=True).exclude(authorized=False).filter(
-            created__lte=cutoff)
+            proj_connections = DataRequestProjectMember.objects.exclude(
+                project__approved=False).exclude(joined=False).filter(
+                member__user__is_active=True).exclude(authorized=False).filter(
+                created__lte=cutoff)
 
-        print("Members: {}".format(members.count()))
-        print("Members with any data connections: {}".format(
-            members_with_data.count()))
-        print("Data connections: {}".format(len(data_connections)))
-        print("Project connections: {}".format(proj_connections.count()))
-        print("Projects drafted: {}".format(projects_made.count()))
-        print("Projects approved: {}".format(projects_approved.count()))
+            """
+            print("Members: {}".format(members.count()))
+            print("Members with data: {}".format(
+                members_with_data.count()))
+            print("Data connections: {}".format(len(data_connections)))
+            print("Project connections: {}".format(proj_connections.count()))
+            print("Projects drafted: {}".format(projects_made.count()))
+            print("Projects approved: {}".format(projects_approved.count()))
+            """
+
+            print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+                date.format('YYYY-MM-DD'),
+                members.count(),
+                members_with_data.count(),
+                len(data_connections),
+                proj_connections.count(),
+                projects_made.count(),
+                projects_approved.count(),
+            ))
