702db7725a46b1b474a21dc560efba1442c209f9
Mad Price Ball <mpball@gmail.com>
2018-06-13 19:13:37 -0700

    Remove legacy app models and perform migrations

--- studies/american_gut/serializers.py
+++ studies/american_gut/serializers.py
@@ -1,16 +1,1 @@
 from rest_framework import serializers
-
-from .models import UserData
-
-
-class UserDataSerializer(serializers.ModelSerializer):
-    """
-    Serializer for American Gut user data.
-    """
-
-    data = serializers.JSONField()
-
-    class Meta:  # noqa: D101
-        model = UserData
-        fields = ('id', 'data')
-        read_only_fields = ('id',)
--- studies/pgp/urls_api.py
+++ studies/pgp/urls_api.py
@@ -2,12 +2,8 @@ from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import HuIdDetail, HuIdList, UserDataDetail
 
 urlpatterns = [
-    url(r'^user-data/$', UserDataDetail.as_view()),
-    url(r'^huids/$', HuIdList.as_view()),
-    url(r'^huids/(?P<pk>(hu)?[0-9A-F]+)/$', HuIdDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
