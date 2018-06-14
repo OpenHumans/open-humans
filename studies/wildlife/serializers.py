014fe31da69a1114c16e91d5316fe62924c0885f
Mad Price Ball <mpball@gmail.com>
2018-06-13 19:13:37 -0700

    Remove legacy app models and perform migrations

--- studies/wildlife/serializers.py
+++ studies/wildlife/serializers.py
@@ -1,17 +1,1 @@
 from rest_framework import serializers
-
-from .models import UserData
-
-
-# TODO: this could be used for American Gut as well
-class UserDataSerializer(serializers.ModelSerializer):
-    """
-    Serializer for Wildlife of Our Home user data.
-    """
-
-    data = serializers.JSONField()
-
-    class Meta:  # noqa: D101
-        model = UserData
-        fields = ('id', 'data')
-        read_only_fields = ('id',)
--- studies/wildlife/views.py
+++ studies/wildlife/views.py
@@ -1,16 +1,1 @@
 from ..views import UserDataDetailView
-
-from .models import UserData
-from .serializers import UserDataSerializer
-
-
-class UserDataDetail(UserDataDetailView):
-    """
-    Detail view for Wildlife of Our Homes user data.
-    """
-
-    def get_queryset(self):
-        return self.get_user_data_queryset()
-
-    user_data_model = UserData
-    serializer_class = UserDataSerializer
