f6ffa78456b44484e36bd631192694f2b52dcae5
Mairi Dulaney <jdulaney@fedoraproject.org>
2018-09-12 17:21:37 -0700

    Refactor withdrawal email just a tad
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- private_sharing/templatetags/private_sharing.py
+++ private_sharing/templatetags/private_sharing.py
@@ -34,18 +34,3 @@ def project_is_connected(project, user):
     Return True if the given project is connected (joined and authorized).
     """
     return project.is_joined(user)
-
-
-@register.simple_tag
-def erasure_requested_txt(erasure_requested):
-    """
-    Returns text requesting that a project erase a member's data.
-    """
-
-    erasure_text = ("""In addition, they have requested erasure of their data.  We have\n"""
-    """deleted the data on our end, and hereby request that you do the same.""")
-
-    if erasure_requested:
-        return erasure_text
-    else:
-        return "The member has not requested that you erase their data"
