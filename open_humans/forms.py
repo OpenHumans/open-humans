fdf36200c7dff846121213507516afe18d0ffc67
Mairi Dulaney <jdulaney@fedoraproject.org>
2018-10-11 12:24:13 -0700

    use reverse instead of resolve
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- open_humans/forms.py
+++ open_humans/forms.py
@@ -256,5 +256,5 @@ class SocialSignupForm(AllauthSocialSignupForm):
         try:
             return super().validate_unique_email(value)
         except forms.ValidationError:
-            redirect('account_login', next=resolve('socialaccount_connections'),
+            redirect('account_login', next=reverse('socialaccount_connections'),
                      socialsignup=True)
