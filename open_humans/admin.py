00da2e3a896974b6a85fbcaf7c384fb5d5c5106d
Mairi Dulaney <jdulaney@fedoraproject.org>
2018-10-16 08:50:05 -0700

    oops
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- open_humans/admin.py
+++ open_humans/admin.py
@@ -5,3 +5,8 @@ from .models import Member, User, GrantProject
 
 admin.site.register(Member)
 admin.site.register(User, UserAdmin)
+
+class GrantProjectAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in GrantProject._meta.get_fields()]
+
+admin.site.register(GrantProject, GrantProjectAdmin)
