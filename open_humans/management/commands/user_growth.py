57263b7f51ee2978f7870155e4363d870e321804
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-11-06 13:55:26 -0800

    create user_growth command

---
+++ open_humans/management/commands/user_growth.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from open_humans.models import Member
+
+
+class Command(BaseCommand):
+    help = 'for plotting user growth'
+    args = ''
+
+    def handle(self, *args, **options):
+        user_number = 1
+        members = Member.objects.order_by(
+            'user__date_joined').filter(user__is_active=True)
+        for member in members:
+            print("{}\t{}".format(
+                member.user.date_joined,
+                user_number
+            ))
+            user_number += 1
