6781d272336aa4d4d7c26738d42b8a21f67dda60
Mairi Dulaney <jdulaney@fedoraproject.org>
2019-01-03 16:04:14 -0800

    rewrite to_representation
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- data_import/serializers.py
+++ data_import/serializers.py
@@ -1,22 +1,17 @@
 from collections import OrderedDict
 
 from rest_framework import serializers
-from rest_framework.relations import PKOnlyObject
 
 from .models import DataFile
 
 
-class DataFileSerializer(serializers.ModelSerializer):
+class DataFileSerializer(serializers.Serializer):
     """
     Serialize a data file.
     """
 
-    metadata = serializers.JSONField()
-
     class Meta:  # noqa: D101
         model = DataFile
-        fields = ('id', 'basename', 'created', 'download_url', 'metadata',
-                  'source')
 
     def to_representation(self, instance):
         """
@@ -25,19 +20,13 @@ class DataFileSerializer(serializers.ModelSerializer):
         keys are created.
         """
         ret = OrderedDict()
-        fields = self._readable_fields
+        fields = ['id', 'basename', 'created', 'download_url', 'metadata',
+                  'source']
         for field in fields:
-            attribute = field.get_attribute(instance)
-
-            check_for_none = attribute.pk if isinstance(
-                attribute, PKOnlyObject) else attribute
-            if check_for_none is None:
-                ret[field.field_name] = None
+            if field == 'download_url':
+                ret[field] = instance.private_download_url(
+                    self.context.get('request', None))
             else:
-                if field.field_name == 'download_url':
-                    ret[field.field_name] = instance.private_download_url(
-                        self.context.get('request', None))
-                else:
-                    ret[field.field_name] = field.to_representation(attribute)
+                ret[field] = getattr(instance, field)
 
         return ret
