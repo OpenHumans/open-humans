0cb848aeb7db41856784d4beed25a4104e2047b0
Mairi Dulaney <jdulaney@fedoraproject.org>
2019-02-08 12:48:25 -0800

    initial celery
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

---
+++ open_humans/celery.py
@@ -0,0 +1,31 @@
+import os
+
+from django.conf import settings
+
+from celery import Celery
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+
+app = Celery('open-humans')
+
+
+app.conf.update({
+    'BROKER_URL': settings.CELERY_BROKER_URL,
+    # Recommended settings. See: https://www.cloudamqp.com/docs/celery.html
+    'BROKER_POOL_LIMIT': None,
+    'BROKER_HEARTBEAT': None,
+    'BROKER_CONNECTION_TIMEOUT': 30,
+    'CELERY_RESULT_BACKEND': settings.CELERY_BROKER_URL,
+    'CELERY_SEND_EVENTS': False,
+    'CELERY_EVENT_QUEUE_EXPIRES': 60,
+})
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
