d7e561a4ab977e23ad63f0c73674b1a76bc6b518
Mairi Dulaney <jdulaney@fedoraproject.org>
2019-04-10 11:39:40 -0700

    change LOG_API_ACCESS_PROJECT_ID to OHLOG_PROJECT_ID
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- data_import/permissions.py
+++ data_import/permissions.py
@@ -9,7 +9,7 @@ class LogAPIAccessAllowed(BasePermission):
     """
 
     def has_permission(self, request, view):
-        if settings.LOG_API_ACCESS_PROJECT_ID:
-            if request.auth.id == int(settings.LOG_API_ACCESS_PROJECT_ID):
+        if settings.OHLOG_PROJECT_ID:
+            if request.auth.id == int(settings.OHLOG_PROJECT_ID):
                 return True
         return False
