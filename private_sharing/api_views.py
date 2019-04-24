80a0ccad90a155dee9ddd184e898f34eabf69862
Mairi Dulaney <jdulaney@fedoraproject.org>
2019-04-24 10:06:42 -0700

    Set queryset order for member-exchange endpoint
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- private_sharing/api_views.py
+++ private_sharing/api_views.py
@@ -159,7 +159,7 @@ class ProjectMemberExchangeView(NeverCacheMixin, ListAPIView):
             sources_shared = self.get_sources_shared(self.obj)
             sources_shared.append(self.obj.project.id_label)
             files = all_files.filter(source__in=sources_shared)
-        return files
+        return files.order_by("source").order_by("file").order_by("id")
 
     def list(self, request, *args, **kwargs):
         """
