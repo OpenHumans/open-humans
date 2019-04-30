5443ec2273f07216c69a03bec760601ba7ac86dc
Mairi Dulaney <jdulaney@fedoraproject.org>
2019-04-30 09:53:06 -0700

    Admin page speedups (#1041)
    * Admin page speedups
    
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>
    
    * Make it easier to find Members in admin (#7)
    
    * Fix projects joined list (fixes #1039)
    
    * remove select_relateds
    
    Signed-off-by: Mairi Dulaney <jdulaney@fedoraproject.org>

--- open_humans/admin.py
+++ open_humans/admin.py
@@ -3,7 +3,17 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Member, User, GrantProject
 
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    """
+    Speed up loading users
+    """
+
+    raw_id_fields = ("user",)
+    search_fields = ("user__username", "user__email")
+
+
+admin.site.register(Member, MemberAdmin)
 admin.site.register(User, UserAdmin)
 
 
--- private_sharing/admin.py
+++ private_sharing/admin.py
@@ -10,11 +10,20 @@ class DataRequestProjectMemberAdmin(admin.ModelAdmin):
 
     readonly_fields = ("created",)
     search_fields = ("member__user__username", "project_member_id", "project__name")
+    raw_id_fields = ("member",)
+
+
+class DataRequestProjectAdmin(admin.ModelAdmin):
+    """
+    set the coordinator field to be raw_id
+    """
+
+    raw_id_fields = ("coordinator",)
 
 
 admin.site.register(models.ProjectDataFile)
-admin.site.register(models.DataRequestProject)
-admin.site.register(models.OAuth2DataRequestProject)
-admin.site.register(models.OnSiteDataRequestProject)
+admin.site.register(models.DataRequestProject, DataRequestProjectAdmin)
+admin.site.register(models.OAuth2DataRequestProject, DataRequestProjectAdmin)
+admin.site.register(models.OnSiteDataRequestProject, DataRequestProjectAdmin)
 admin.site.register(models.DataRequestProjectMember, DataRequestProjectMemberAdmin)
 admin.site.register(models.FeaturedProject)
